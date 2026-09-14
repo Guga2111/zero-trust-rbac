edc2e0cf1cd6b3935f8c7b5c6d93ee3e646f89e4
Guga2111 <luisgosampaio@gmail.com>
2026-09-14 09:13:13 -0300

    chore: spring project setup

---
+++ rbac/src/test/java/com/zerotrust/rbac/RbacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerotrust.rbac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
